7075a1b736a32e2d2b9bdfec878bdb35dfad2efe
Madara48sk <matiyastewodros63@gmail.com>
2024-12-08 18:59:23 +0300

    initial commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-
+// src/components/UserProfile.js
 function UserProfile() {
   return (
-    <div className="mx-auto my-20 max-w-sm bg-gray-100 p-8 rounded-lg shadow-lg">
+    <div className="bg-gray-100 rounded-lg shadow-lg mx-auto my-10 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="w-36 h-36 mx-auto rounded-full"
+        className="rounded-full mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
       />
-      <h1 className="text-xl text-blue-800 my-4 text-center">John Doe</h1>
-      <p className="text-gray-600 text-center text-base">
+      <h1 className="text-lg sm:text-lg md:text-xl text-blue-800 my-4 text-center">
+        John Doe
+      </h1>
+      <p className="text-sm sm:text-sm md:text-base text-gray-600 text-center">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
