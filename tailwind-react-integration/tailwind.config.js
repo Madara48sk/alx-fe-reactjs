2a515be3457751031dd7205a80eb29bcc9a75f79
Madara48sk <matiyastewodros63@gmail.com>
2024-12-08 17:23:17 +0300

    initial commit

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", //Make sure this path is correct for your project
-    "./index.html",                 //Make sure this path is correct for your project
+    "./src/**/*.{js,jsx,ts,tsx}", // Path to your React components
+    "./index.html",                // Path to your main HTML file
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: 'media', // or 'class'
+  variants: {
+    extend: {},
+  },
 };
