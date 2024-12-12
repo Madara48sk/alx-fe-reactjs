4d2398f4e28fa83650aa9f8ba80b25c71f09bdbd
Madara48sk <matiyastewodros63@gmail.com>
2024-12-12 14:33:01 +0300

    initial commit

---
+++ github-user-search/src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <>
+    <header>
+        <h1>GitHub User Search</h1>
+    </header>
+    <main>
+        <p>Ready to search for GitHub users!</p>
+    </main>
+    </>
+  );
+}
+
+export default Main;
