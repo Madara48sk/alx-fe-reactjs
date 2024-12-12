9c0a22f1e9f97f8a324cd4dab4ff83efe5291e43
Madara48sk <matiyastewodros63@gmail.com>
2024-12-12 14:52:01 +0300

    initial commit

--- github-user-search/src/main.jsx
+++ github-user-search/src/main.jsx
@@ -7,28 +7,31 @@ function Main() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  
+
   const handleSearch = async (username) => {
-      setUser(null);
-      setError(false);
-      setLoading(true);
-      const userData = await githubService.fetchUserData(username);
-      setLoading(false);
-      if (userData) {
+    setUser(null);
+    setError(false);
+    setLoading(true);
+    const userData = await githubService.fetchUserData(username);
+    setLoading(false);
+    if (userData) {
       setUser(userData);
-      } else {
+    } else {
       setError(true);
-      }
+    }
   };
-  
+
   return (
-      <>
+    <>
       <header>
-          <h1>GitHub User Search</h1>
+        <h1>GitHub User Search</h1>
       </header>
       <main>
-          <SearchInput onSearch={handleSearch} />
-          <UserCard user={user} loading={loading} error={error} />
+        <SearchInput onSearch={handleSearch} />
+        <UserCard user={user} loading={loading} error={error} />
       </main>
-      </>
+    </>
   );
+}
+
+export default Main;
