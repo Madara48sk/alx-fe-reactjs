7c4f4e1235c0f10bc429abfb77eee42a8f842319
Madara48sk <matiyastewodros63@gmail.com>
2024-12-15 21:13:10 +0300

    initial commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,15 +1,32 @@
 import axios from 'axios';
 
 const GITHUB_API_URL = 'https://api.github.com/users';
+const GITHUB_SEARCH_API_URL = 'https://api.github.com/search/users?q=';
 
 const fetchUserData = async (username) => {
-    try {
+  try {
     const response = await axios.get(`${GITHUB_API_URL}/${username}`);
     return response.data;
-    } catch (error) {
+  } catch (error) {
     console.error('Error fetching user', error);
     return null;
-    }
+  }
 };
 
-export default { fetchUserData };
+const fetchUsersByCriteria = async (username, location, minRepos) => {
+    let query = username ? `user:${username}` : '';
+    if (location) {
+      query += ` location:${location}`;
+    }
+    if (minRepos) {
+        query += ` repos:>${minRepos}`
+    }
+    try{
+        const response = await axios.get(`${GITHUB_SEARCH_API_URL}${query}`);
+        return response.data;
+    } catch(error) {
+        console.error('Error fetching user by criteria', error)
+        return null;
+    }
+}
+export default { fetchUserData, fetchUsersByCriteria };
